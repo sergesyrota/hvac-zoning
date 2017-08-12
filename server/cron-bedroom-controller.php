74f62c3975d878edb1648af935452c5318657c83
Sergey Syrota <serg.syrota@gmail.com>
2017-08-12 15:30:19 -0500

    WIP commit

--- server/cron-bedroom-controller.php
+++ server/cron-bedroom-controller.php
@@ -10,6 +10,7 @@ define('DEBUG', false);
 define('ACTION', true);
 
 require_once '/var/www/home/dashboard/include/rs485.php';
+require_once '/home/sergey/temps/hvac-zoning/server/middleTstat.php';
 
 // Degrees from most open to most closed.
 $degrees = [
@@ -27,7 +28,7 @@ $roomVents = [
 // AC or Heat
 $mode = null;
 // Get thermostat mode
-$thermostat = json_decode(`curl http://192.168.8.90/tstat/ 2>/dev/null`);
+$thermostat = getMiddleTstat();
 if ($thermostat->tmode == 2) {
     $mode = 'cool';
 } else {
@@ -85,7 +86,7 @@ foreach ($degrees as $positionName=>$deg) {
         if ($targetVentState[$roomName] == $positionName) {
             foreach ($vents as $vent) {
                 debug("Setting $roomName $vent at $deg");
-                if (ACTION) {
+                if (ACTION && $vent != 'GuestVent') {
                     tryCmd($vent, 'setDegrees:' . $deg);
                 }
                 sleep(3);
@@ -106,8 +107,28 @@ function ftoc($f) {
 }
 
 function getTargetGuestTemperature($mode) {
+    $hour = (int)date('H');
     if ($mode == 'cool') {
-        return ftoc(73);
+        switch (date('N')) {
+            case 1: // Mon
+            case 2: // Tue
+            case 3: // Wed
+            case 4: // Thu
+            case 5: // Fri
+                if ($hour>=13 && $hour<16) {
+                    return ftoc(74);
+                }
+                // Fall through to default, as only on weekdays we override nap time for Andrew
+            default:
+                // Same schedule for all weekdays
+                // 22-5: 21, 5-8: 22, 8-16: 20, 16-22: 22
+                if ($hour<7 || $hour>=20) {
+                    return ftoc(72);
+                } else {
+                    return ftoc(78);
+                }
+                return;
+        }
     } else {
         return ftoc(71);
     }
@@ -126,11 +147,9 @@ function getTargetTemperature($mode)
 function getTargetHeatTemperature()
 {
     $hour = (int)date('H');
-    // Day off
-    if (time() < strtotime('2017-04-08 00:00:00')) {
+    // Vacation
+    if (time() < strtotime('2017-06-23 16:00:00')) {
         return ftoc(62);
-    } elseif (time() < strtotime('2017-04-08 15:00:00')) {
-        return ftoc(76);
     }
     switch (date('N')) {
         case 1: // Mon
@@ -163,6 +182,10 @@ function getTargetHeatTemperature()
 // This is hardcoded schedule; Target temp in C
 function getTargetCoolTemperature()
 {
+    // guest bedroom work
+    if (time() < strtotime('2017-07-25 18:00:00')) {
+        return ftoc(71);
+    }
     $hour = (int)date('H');
     switch (date('N')) {
         case 1: // Mon
@@ -172,9 +195,12 @@ function getTargetCoolTemperature()
         case 5: // Fri
             // Same schedule for all weekdays
             // 22-5: 23, 5-8: 24.5, 8-16: 28, 16-22: 24.5
-            if ($hour<5 || $hour>=22) {
-                return 23;
-            } elseif($hour<8 || $hour>=16) {
+            if ('2017-07-05' == date('Y-m-d')) {
+                return 22;
+            }
+            if ($hour<5 || $hour>=21) {
+                return 22;
+            } elseif($hour<8 || $hour>=20) {
                 return 24.5;
             } else {
                 return 28;
@@ -184,7 +210,7 @@ function getTargetCoolTemperature()
         case 7: // Sun
             // Same schedule for both days of weekend
             if ($hour<7 || $hour>=22) {
-                return 23;
+                return 22;
             } else {
                 return 24.5;
             }
@@ -206,3 +232,18 @@ function tryCmd($device, $command, $attempts=3) {
     }
     throw $lastException;
 }
+
+// Make several attempts to get tstat data, as sometimes it's not working.
+/*function getMiddleTstat() {
+    for ($i=0; $i<5; $i++) {
+        $data = json_decode(`curl -m 60 http://192.168.8.90/tstat/ 2>/dev/null`);
+        if ($data !== null && !empty($data->tmode)) {
+            if (($data->tmode == 2 && !empty($data->t_cool)) || !empty($data->t_heat)) {
+                return $data;
+            }
+        }
+        sleep(4);
+    }
+    throw new Exception("Cannot get thermostat data using curl.");
+}
+*/
